a32b14c0777bc7e16956b7b75d54fb81edbfeef4
Blufe <bluephoenixlab@gmail.com>
2015-10-29 13:43:28 +0900

    apcのインストールをapcuにも対応

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 include_attribute "phpenv"
 
-default["phpenv"]["apc"]["src_dir"] = "/usr/local/src"
-default["phpenv"]["apc"]["version"] = "3.1.13"
+default["phpenv"]["apc"]["src_dir"]     = "/usr/local/src"
+default["phpenv"]["apc"]["module_name"] = "apcu"
+default["phpenv"]["apc"]["version"]     = "4.0.7"
